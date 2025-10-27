ba325da4296065d0b897e9d41662b4baef34df4f
Tiyasa Sil <siltiyasa14@gmail.com>
2025-10-27 20:01:48 +0530

    Add files via upload

---
+++ HackerRank/Problem13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    //Write your logic to print the tokens of the sentence here.
+    for (char *c = s; *c != NULL; c++) {
+    if (*c == ' ') {
+        *c = '\n';
+    }
+}
+printf("%s", s);
+    return 0;
+}
